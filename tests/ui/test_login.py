0cf656931cd4525c1e59284c0475504c0ec486b7
Narma-Ram <emailnarma@gmail.com>
2026-08-28 17:47:12 -0500

    Run login tests in headless mode

--- tests/ui/test_login.py
+++ tests/ui/test_login.py
@@ -61,7 +61,7 @@ def test_inactive_user_cannot_login():
     inactive_user = test_data["inactive_user"]
 
     with sync_playwright() as p:
-        browser = p.chromium.launch(headless=False)
+        browser = p.chromium.launch(headless=True)
         page = browser.new_page()
 
         login_page = LoginPage(page)
